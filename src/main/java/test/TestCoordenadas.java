5a0ca99c02ce6e3ce08742eaa71ba88ffe6b8d2c
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-06-25 15:45:30 -0500

    v2

--- src/main/java/test/TestCoordenadas.java
+++ src/main/java/test/TestCoordenadas.java
@@ -9,7 +9,6 @@ public class TestCoordenadas {
     static Scanner datos = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Random radon = new Random();
 
         System.out.println("Cuántos movimientos desea realizar:");
         int n = datos.nextInt();
@@ -22,24 +21,45 @@ public class TestCoordenadas {
 
     }
 
-    public static void pedirCoordenadas (Coordenadas[] coor ) {
-        Random randon = new Random();
-        int x,y; 
+    public static void pedirCoordenadas(Coordenadas[] coor) {
+        Random aleatorio = new Random();
+        int x, y;
 
-        for (int i = 0; i < coor .length; i++) {
-            datos.nextLine();
-            System.out.println("Punto Nro :"+(i+1));
-            System.out.println("Coordenada en x:");
-            x = datos.nextInt();
-            System.out.println("Coordenada en y:");
-            y = datos.nextInt();
-            coor [i] = new Coordenadas(x,y);
+        int opcion;
+        int mover = (int) (Math.random() * 4 + 1);
+        datos.nextLine();
+        System.out.println("Punto inicial");
+        System.out.println("Coordenada en x:");
+        x = datos.nextInt();
+        System.out.println("Coordenada en y:");
+        y = datos.nextInt();
+        for (int i = 0; i < coor.length; i++) {
+
+            coor[i] = new Coordenadas(x, y);
+            System.out.println("Digite el nùmero del movimiento que desea para el punto ");
+            System.out.println("1.-Arriba");
+            System.out.println("2.-Abajo");
+            System.out.println("3.-Derecha");
+            System.out.println("4.-Izquierda");
+            opcion = datos.nextInt();
+
+            if (opcion == 1) {
+                y = y + mover;
+                System.out.println("x: "+x+",y: "+y);
+            } else if (opcion == 2) {
+                y = y - mover;
+                System.out.println("x: "+x+",y: "+y);
+            } else if (opcion == 3) {
+                x = x + mover;
+                System.out.println("x: "+x+",y: "+y);
+            } else if (opcion == 4) {
+                x = x - mover;
+                System.out.println("x: "+x+",y: "+y);
+            } else {
+                System.out.println("ERROR");
+            }
+            coor[i]= new Coordenadas(x,y);
         }
-    }
-    
-    public static void dirigirCoordenadas(){
-    
-    
-    
+
     }
 }
